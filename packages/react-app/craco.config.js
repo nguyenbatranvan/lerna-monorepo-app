6a52f038336e653cae0ab03e12031402d4609b41
nguyenbatranvan <nguyenbatranvan96@gmail.com>
2022-09-20 17:46:45 +0700

    update alias

--- packages/react-app/craco.config.js
+++ packages/react-app/craco.config.js
@@ -1,26 +1,46 @@
-const path = require("path");
 const {getLoader, loaderByName} = require("@craco/craco");
 const packages = [];
-packages.push(path.join(__dirname, "../shared"));
+// packages.push(path.join(__dirname, "../shared"));
+const path = require("path");
+const fs = require("fs");
+const CracoAlias = require('craco-alias');
+const rewireBabelLoader = require("craco-babel-loader-plugin");
+
+// helpers
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
 module.exports = {
+    plugins: [
+        //This is a craco plugin: https://github.com/sharegate/craco/blob/master/packages/craco/README.md#configuration-overview
+        { plugin: rewireBabelLoader,
+            options: {
+                includes: [path.join(__dirname, "../shared")], //put things you want to include in array here
+            }
+        },
+        {
+            plugin: CracoAlias,
+            options: {
+                source: 'tsconfig',
+                tsConfigPath: './tsconfig.paths.json',
+            }
+        }
+    ],
     devServer: {
         port: 4000
     },
-    webpack: {
-        configure: (webpackConfig, arg) => {
-            const {isFound, match} = getLoader(webpackConfig, loaderByName("babel-loader"));
-            if (isFound) {
-                const include = Array.isArray(match.loader.include)
-                    ? match.loader.include
-                    : [match.loader.include];
-
-                match.loader.include = include.concat(packages);
-            }
-
-            // webpackConfig.resolve.alias={
-            //     "@components": path.join(__dirname, '../shared/src/components')
-            // }
-            return webpackConfig;
-        },
-    },
+    // webpack: {
+    //     configure: (webpackConfig, arg) => {
+    //         const {isFound, match} = getLoader(webpackConfig, loaderByName("babel-loader"));
+    //         if (isFound) {
+    //             const include = Array.isArray(match.loader.include)
+    //                 ? match.loader.include
+    //                 : [match.loader.include];
+    //
+    //             match.loader.include = include.concat(packages);
+    //         }
+    //         return webpackConfig;
+    //     },
+    // },
 };
--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,9 +1,9 @@
+import {CardPage} from "@page/Card";
+import {Home} from "@page/home";
 import React from 'react';
 import {FiCreditCard, FiHome} from "react-icons/fi";
 import {Outlet, Route, Routes, useNavigate} from "react-router-dom";
 import Layout, {LinkItemProps} from "shared/src/layout/layout";
-import {CardPage} from "./pages/Card";
-import {Home} from "./pages/home";
 
 function App() {
     const menus: LinkItemProps[] = [{
